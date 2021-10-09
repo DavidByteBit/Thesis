f465c26876a2365d5f8122f051c193708c432f5a
David <David>
2021-10-09 16:41:46 -0700

    g

--- forward.py
+++ forward.py
@@ -205,11 +205,16 @@ def dot_2d(x,y):
     res = sfix.Array(1)
     res[0] = sfix(0)
 
+    assert len(x) == len(y)
+    assert len(x[0]) == len(y[0])
+
     @for_range(len(x))
     def _(i):
         @for_range(len(x[0]))
         def _(j):
-            res[0] += x[i][j] * y[i][j]
+            prod = x[i][j] * y[i][j]
+            print_ln(prod)
+            res[0] += prod
 
     return res[0]
 
