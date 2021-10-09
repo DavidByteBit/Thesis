ab18cf2f57d324cf7801d9b2395d26c14e5c6227
David <David>
2021-10-09 16:42:57 -0700

    g

--- forward.py
+++ forward.py
@@ -213,7 +213,7 @@ def dot_2d(x,y):
         @for_range(len(x[0]))
         def _(j):
             prod = x[i][j] * y[i][j]
-            print_ln("%s", prod)
+            print_ln("%s", prod.reveal())
             res[0] += prod
 
     return res[0]
